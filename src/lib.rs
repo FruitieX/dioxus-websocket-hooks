35b6904f29115ddd8afe8c5c31f150d1eb4745da
Rasmus Lövegren <fruitiex@gmail.com>
2022-01-09 20:58:21 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,88 @@
+use std::{
+    rc::Rc,
+    sync::{Arc, RwLock},
+};
+
+use dioxus::prelude::*;
+use futures::{
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt,
+};
+use reqwasm::websocket::{futures::WebSocket, Message};
+use serde::{Deserialize, Serialize};
+use wasm_bindgen_futures::spawn_local;
+
+pub struct DioxusWs {
+    sender: Arc<RwLock<SplitSink<WebSocket, Message>>>,
+    receiver: Arc<RwLock<SplitStream<WebSocket>>>,
+}
+
+impl DioxusWs {
+    pub fn new(url: &str) -> DioxusWs {
+        let ws = WebSocket::open(url).unwrap();
+
+        let (sender, receiver) = ws.split();
+        let sender = Arc::new(RwLock::new(sender));
+        let receiver = Arc::new(RwLock::new(receiver));
+
+        DioxusWs { sender, receiver }
+    }
+
+    pub fn send(&self, msg: Message) {
+        let sender = self.sender.clone();
+
+        spawn_local(async move {
+            let mut sender = sender.write().unwrap();
+            sender.send(msg).await.unwrap()
+        });
+    }
+
+    pub fn send_text(&self, text: String) {
+        let msg = Message::Text(text);
+        self.send(msg);
+    }
+
+    pub fn send_json<T: Serialize>(&self, value: &T) {
+        let json = serde_json::to_string(value).unwrap();
+        let msg = Message::Text(json);
+
+        self.send(msg)
+    }
+}
+
+pub fn use_ws_context_provider(cx: &ScopeState, url: &str, reducer: impl Fn(Message) + 'static) {
+    let reducer = Rc::new(reducer);
+
+    cx.use_hook(|_| {
+        let ws = cx.provide_context(DioxusWs::new(url));
+        let receiver = ws.receiver.clone();
+
+        cx.push_future(async move {
+            let mut receiver = receiver.write().unwrap();
+            while let Some(msg) = receiver.next().await {
+                if let Ok(msg) = msg {
+                    reducer(msg);
+                }
+            }
+        })
+    });
+}
+
+pub fn use_ws_context_provider_json<T>(cx: &ScopeState, url: &str, reducer: impl Fn(T) + 'static)
+where
+    T: for<'de> Deserialize<'de>,
+{
+    let reducer = move |msg| match msg {
+        Message::Text(text) => {
+            let json = serde_json::from_str::<T>(&text).unwrap();
+            reducer(json)
+        }
+        Message::Bytes(_) => {}
+    };
+
+    use_ws_context_provider(cx, url, reducer)
+}
+
+pub fn use_ws_context(cx: &ScopeState) -> Rc<DioxusWs> {
+    cx.consume_context::<DioxusWs>().unwrap()
+}
